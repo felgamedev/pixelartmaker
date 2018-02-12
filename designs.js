d473f999b4a473786141c70e160e7a29120992d0
felgamedev <felgamedev@gmail.com>
2018-02-11 21:23:02 -0400

    Prevent submit default action and call makeGrid

--- designs.js
+++ designs.js
@@ -5,6 +5,11 @@ const canvasHeight = document.querySelector('#inputHeight');
 const form = document.querySelector('#sizePicker');
 const table = document.querySelector('#pixelCanvas');
 
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  makeGrid();
+});
+
 function makeGrid() {
 
 // Your code goes here!
